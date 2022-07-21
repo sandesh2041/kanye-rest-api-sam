5fb95aa5339072151370ad79fc6709340c4f3c09
Sam <sandeshmas@gmail.com>
2022-07-20 18:24:13 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 //I now have an express app/server that can receive http(s) requests and send back http(s) responses
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
 app.use(cors());
